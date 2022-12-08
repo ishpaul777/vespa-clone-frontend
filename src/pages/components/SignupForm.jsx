1f9ab4aad547cf96c8f958c2ecd1e3e878c74e0f
looh16 <langacustodio@gmail.com>
2022-12-08 20:11:35 +0200

    Add style to sigup form

--- src/pages/components/SignupForm.jsx
+++ src/pages/components/SignupForm.jsx
@@ -76,49 +76,58 @@ function SignupForm() {
 	};
 
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<div>
+		<div className="Auth-container">
+			<form onSubmit={handleSubmit} className="Auth-form">
+			<div className="Auth-content">
+			<h3 className="Auth-title">Sign Up</h3>
+				<div className="form-group mt-3 text-start">
 					<label htmlFor="email">Name</label>
 					<input
 						type="name"
+						className="form-control mt-1"
 						name="name"
 						id="name"
 						value={formData.name}
 						onChange={handleChange}
 					/>
 				</div>
-				<div>
+				<div className="form-group mt-3 text-start">
 					<label htmlFor="email">Email</label>
 					<input
 						type="email"
+						className="form-control mt-1"
 						name="email"
 						id="email"
 						value={formData.email}
 						onChange={handleChange}
 					/>
 				</div>
-				<div>
+				<div className="form-group mt-3 text-start">
 					<label htmlFor="password">Password</label>
 					<input
 						type="password"
+						className="form-control mt-1"
 						name="password"
 						id="password"
 						value={formData.password}
 						onChange={handleChange}
 					/>
 				</div>
-				<div>
+				<div className="form-group mt-3 text-start">
 					<label htmlFor="password_confirmation">Confirm Password</label>
 					<input
 						type="password"
+						className="form-control mt-1"
 						name="password_confirmation"
 						id="password_confirmation"
 						value={formData.password_confirmation}
 						onChange={handleChange}
 					/>
 				</div>
-				<button type="submit">Sign Up</button>
+				</div>
+				<div className="form-group mt-3 ">
+				<button type="submit" className="btn btn-primary ">Submit</button>
+				</div>
 			</form>
 		</div>
 	)
