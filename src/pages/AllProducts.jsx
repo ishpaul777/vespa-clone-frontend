17d7784faf2621b86e0ada744025f7407dd27119
looh16 <langacustodio@gmail.com>
2022-12-12 20:58:17 +0200

    Add styles

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -7,6 +7,7 @@ import "../styles/carrocel.css";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts } from "../redux/products/products_reducer";
+import { FaBorderNone, FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
 
 const AllProducts = () => {
   const data = useSelector((state) => state.products);
@@ -32,40 +33,47 @@ const AllProducts = () => {
   if (!data || !data.length) return null;
 
   return (
-    <div className="container">
-      <div>
-        <h1>Our Products</h1>
+    <div className="container mt-5">
+      <div className="d-flex justify-content-center">
+        <h1>Last Products</h1>
       </div>
-      <div className="container d-flex carousel" ref={carousel}>
-        {data.map((item) => {
-          const { id, name, image_url, description } = item;
-          return (
-            <div className="item" key={id}>
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src={image_url} alt={name} />
-                <Card.Body>
-                  <Card.Title>{name}</Card.Title>
-                  <Card.Text>{description}</Card.Text>
-                  <Button variant="primary">See Details</Button>
-                </Card.Body>
-              </Card>
-            </div>
-          );
-        })}
+      <div className="d-flex justify-content-center">
+        <h6>Please select a Product</h6>
       </div>
-      <div className="buttons">
-        <button onClick={handleLeftClick}>
-          <img
-            src="/static/images/216151_right_chevron_icon.png"
-            alt="Scroll Left"
-          />
-        </button>
-        <button onClick={handleRightClick}>
-          <img
-            src="/static/images/216151_right_chevron_icon.png"
-            alt="Scroll Right"
-          />
-        </button>
+      <div className="d-flex mt-5">
+        <div className="arrows">
+          <button onClick={handleRightClick}>
+            <FaChevronCircleLeft size={40} fill='#97bf0f'/>
+          </button>
+        </div>
+        <div className="d-flex carousel" ref={carousel}>
+
+          {data.map((item) => {
+            const { id, name, image_url, description } = item;
+            return (
+              <div className="container d-flex justify-content-center" key={id}>
+                <Card style={{ width: "14rem" }}>
+                  <Card.Img variant="top" src={image_url} alt={name} />
+                  <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>{description}</Card.Text>
+                    <Button variant="primary">See Details</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            );
+          })}
+
+        </div>
+
+        <div className="arrows">
+          <button onClick={handleLeftClick}>
+            <FaChevronCircleRight size={40} fill='#97bf0f' style={{
+    background: '#fff',
+    border: 0
+    }}/>
+          </button>
+        </div>
       </div>
     </div>
   );
