96091717bf10fcec2a36451cfe571f619282ee7b
looh16 <langacustodio@gmail.com>
2022-12-12 21:51:51 +0200

    Add styles

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import CardGroup from 'react-bootstrap/CardGroup';
 import "../styles/carrocel.css";
 
 import React from "react";
@@ -32,6 +33,7 @@ const AllProducts = () => {
 
   if (!data || !data.length) return null;
 
+
   return (
     <div className="container mt-5">
       <div className="d-flex justify-content-center">
@@ -43,35 +45,41 @@ const AllProducts = () => {
       <div className="d-flex mt-5">
         <div className="arrows">
           <button onClick={handleRightClick}>
-            <FaChevronCircleLeft size={40} fill='#97bf0f'/>
+            <FaChevronCircleLeft size={40} fill='#97bf0f' />
           </button>
         </div>
         <div className="d-flex carousel" ref={carousel}>
 
           {data.map((item) => {
-            const { id, name, image_url, description } = item;
+            const { id, model, image_url, description } = item;
             return (
               <div className="container d-flex justify-content-center" key={id}>
-                <Card style={{ width: "14rem" }}>
-                  <Card.Img variant="top" src={image_url} alt={name} />
-                  <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>{description}</Card.Text>
-                    <Button variant="primary">See Details</Button>
-                  </Card.Body>
-                </Card>
+                <CardGroup>
+                  <Card className="border border-1 shadow p-2 mb-2 bg-body rounded" style={{ width: "14rem" }} >
+                    <Card.Img variant="top" src={image_url} alt={model} />
+                    <Card.Body className="col text-center">
+                      <Card.Title>{model}</Card.Title>
+                      <Card.Text>{description}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                  </Card>
+                </CardGroup>
               </div>
             );
           })}
 
         </div>
 
+
+
         <div className="arrows">
           <button onClick={handleLeftClick}>
             <FaChevronCircleRight size={40} fill='#97bf0f' style={{
-    background: '#fff',
-    border: 0
-    }}/>
+              background: '#fff',
+              border: 0
+            }} />
           </button>
         </div>
       </div>
