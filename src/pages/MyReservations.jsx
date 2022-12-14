5ee5fc90659061b979c19853142cefd414af98e6
Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>
2022-12-15 02:02:20 +0530

    added cta when no reservations

--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -3,43 +3,61 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllReservedProducts } from '../redux/reservations/reservations_reducer';
 import Table from 'react-bootstrap/Table';
+import { Link } from 'react-router-dom';
 
 const MyReservations = () => {
-    const data = useSelector((state) => state.reservations);
-    const dispatch = useDispatch();
+  const data = useSelector((state) => state.reservations);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (!data.length) {
-            dispatch(getAllReservedProducts());
+  useEffect(() => {
+    if (!data.length) {
+      dispatch(getAllReservedProducts());
+    }
+  }, [dispatch]);
+
+  return (
+    <div className='container d-flex-column'>
+      <h1 className="text-center mt-5"> MY RESERVATIONS </h1>
+      <Table striped bordered hover size="sm" className='mt-5'>
+        <thead>
+          <tr>
+            <th>CITY</th>
+            <th>DATE OF RESERVATION</th>
+            <th>PRODUCT NAME</th>
+            <th>PRODUCT COLOR</th>
+          </tr>
+        </thead>
+        {data.length > 0
+          ?
+          data.map((item, index) => (
+            <tbody>
+              <tr key={index}>
+                <td>{item.city}</td>
+                <td>{item.reserved_date}</td>
+                <td>product name</td>
+                <td>product color</td>
+              </tr>
+            </tbody>
+          ))
+          : <tbody className='text-center'>
+            <tr>
+              <td
+                colSpan='4'
+              >
+                No reservations yet
+                <Link to='/reserve' className="mt-3 btn btn-primary w-20 m-auto d-block">
+                  Add +
+                </Link>
+              </td>
+
+            </tr>
+          </tbody>
         }
-    }, [dispatch]);
 
-    return (
-      <div className='container d-flex-column'>
-        <h1 className="text-center mt-5"> MY RESERVATIONS </h1>
-         <Table striped bordered hover size="sm" className='mt-5'>
-      <thead>
-        <tr>
-          <th>CITY</th>
-          <th>DATE OF RESERVATION</th>
-          <th>PRODUCT NAME</th>
-          <th>PRODUCT COLOR</th>
-        </tr>
-      </thead>
-      {data.map((item, index) => (
-             <tbody>
-             <tr key={index}>
-              <td>{item.city}</td>
-               <td>{item.reserved_date}</td>
-               <td>product name</td>
-               <td>product color</td>
-             </tr>
-           </tbody>
-      ))}
-     
-    </Table>
-      </div>
-    );
+
+      </Table>
+    </div>
+  );
 };
 
 export default MyReservations;
