294a40ea1c68373cd8f4b761c1f2bdb53e5065a5
looh16 <langacustodio@gmail.com>
2022-12-14 16:40:16 +0200

    Add reservations

--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllReservedProducts } from '../redux/reservations/reservations_reducer';
+import Table from 'react-bootstrap/Table';
 
 const MyReservations = () => {
     const data = useSelector((state) => state.reservations);
@@ -17,7 +18,27 @@ const MyReservations = () => {
 
     return (
       <div>
-         
+         <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>CITY</th>
+          <th>DATE OF RESERVATION</th>
+          <th>PRODUCT NAME</th>
+          <th>PRODUCT COLOR</th>
+        </tr>
+      </thead>
+      {data.map((item, index) => (
+             <tbody>
+             <tr>
+              <td>{item.city}</td>
+               <td>{item.reserved_date}</td>
+               <td>product name</td>
+               <td>product color</td>
+             </tr>
+           </tbody>
+      ))}
+     
+    </Table>
       </div>
     );
 };
